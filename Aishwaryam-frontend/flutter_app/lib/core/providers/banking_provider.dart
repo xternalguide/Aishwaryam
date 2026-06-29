c96a472d52891f73f4fa50e7c1f9cc8d06ea914f
P. Sri Venkatesh <srivenkatesh118@gmail.com>
2026-06-29 15:09:21 +0530

    chore: remove unused Aishwaryam-android and Aishwaryam-frontend codebases

--- Aishwaryam-frontend/flutter_app/lib/core/providers/banking_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../data/repositories/banking_repository.dart';
-
-final bankAccountsProvider = FutureProvider.autoDispose<List<Map<String, dynamic>>>((ref) async {
-  return await bankingRepository.getBankAccounts();
-});
--- Aishwaryam-frontend/flutter_app/lib/core/providers/wallet_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../data/repositories/wallet_repository.dart';
-
-final walletBalanceProvider = FutureProvider<int>((ref) async {
-  final repo = ref.watch(Provider((ref) => walletRepository));
-  return await repo.getBalance();
-});
